c29313d3b0fbe0e85ae75b0b7162f10d76e1413e
duyuqian <809009017@qq.com>
2020-08-19 10:54:46 +0800

    [duyuqian] feat: get the data from url

--- app/src/main/java/com/duyuqian/network/MainActivity.java
+++ app/src/main/java/com/duyuqian/network/MainActivity.java
@@ -3,12 +3,75 @@ package com.duyuqian.network;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    public static String URL = "https://twc-android-bootcamp.github.io/fake-data/data/default.json";
+    OkHttpClient okHttpClient = new OkHttpClient();
+    Toast toast;
+    Request request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+
+    @OnClick(R.id.get_data)
+    public void onClick() {
+        getDataFromURL(URL);
+    }
+
+
+    public void getDataFromURL(String url) {
+        request = new Request.Builder()
+                .url(url)
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(
+                new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        showToast(toast, "failed");
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            final String result = response.body().string();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    showToast(toast, result);
+                                }
+                            });
+                        }
+                    }
+                }
+        );
+    }
+
+    public void showToast(Toast toast, String content) {
+        if (toast == null) {
+            toast = Toast.makeText(this, content, Toast.LENGTH_SHORT);
+        } else {
+            toast.setText(content);
+        }
+        toast.show();
+    }
+
 }
