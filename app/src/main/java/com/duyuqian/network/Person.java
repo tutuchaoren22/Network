7c387357cb8aeb5ab7bee6a132baabd323dba5a4
duyuqian <809009017@qq.com>
2020-08-19 11:04:49 +0800

    [duyuqian] feat: create the model

---
+++ app/src/main/java/com/duyuqian/network/Person.java
@@ -0,0 +1,27 @@
+package com.duyuqian.network;
+
+public class Person {
+    private String name;
+    private String avatar;
+
+    public Person(String name, String avatar) {
+        this.name = name;
+        this.avatar = avatar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
---
+++ app/src/main/java/com/duyuqian/network/Wrapper.java
@@ -0,0 +1,19 @@
+package com.duyuqian.network;
+
+import java.util.List;
+
+public class Wrapper {
+    private List<Person> data;
+
+    public Wrapper(List<Person> data) {
+        this.data = data;
+    }
+
+    public List<Person> getData() {
+        return data;
+    }
+
+    public void setData(List<Person> data) {
+        this.data = data;
+    }
+}
